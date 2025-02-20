bacdb017ff245f4033e4ccf357900c51dd009e25
Erasmo Jr. <erasmo.cjunior@gmail.com>
2025-02-20 12:46:34 -0300

    feat: add 2px of margin (layer_stats & bongo_cat)

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -54,7 +54,7 @@ lv_obj_t *zmk_display_status_screen() {
 
     zmk_widget_layer_status_init(&layer_stats_widget, screen);
     // lv_obj_align(zmk_widget_layer_stats_obj(&layer_stats_widget), LV_ALIGN_BOTTOM_LEFT, 2, -18);
-    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_stats_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
+    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_stats_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_MID, 0, 2);
 
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
