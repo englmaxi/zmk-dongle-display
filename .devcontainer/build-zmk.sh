8e6f6b752238f7e9f6545fc23e3591146fd853d0
Fabian Grutschus <github@lubyte.de>
2026-01-12 19:44:30 +0100

    Allow passing the path to config file

--- .devcontainer/build-zmk.sh
+++ .devcontainer/build-zmk.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 set -e # to exit the subshell as soon as an error happens
+set -x
 
-build_dir=$(mktemp -d)
+build_dir=$(mktemp -d -p "/tmp" zmk-build.XXXXXX)
 
 board_name=$1
 shield_name=$2
 shield_file_name=$(echo $shield_name | tr ' ' '_')
 
 export zmk_load_arg=" -DZMK_EXTRA_MODULES='${WORKSPACE_PATH}' "
-new_tmp_dir="${TMPDIR:-/tmp}/zmk-config"
+new_tmp_dir=$(mktemp -d -p "/tmp" zmk-config.XXXXXX)
 mkdir -p "${new_tmp_dir}"
 base_dir=${new_tmp_dir}
+
+config_dir="${CONFIG_DIR:-config}"
 
 rm -rf $base_dir/.west
 
 mkdir -p $base_dir/config/
-cp -R $WORKSPACE_PATH/config/* $base_dir/config/
+cp -R $WORKSPACE_PATH/${config_dir}/* $base_dir/config/
 
 cd $base_dir
 
